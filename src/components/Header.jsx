bbe63935ccbb0008efc8e1b0142b521845b11832
Cid Oliveira <alcides.angelo10@gmail.com>
2025-07-09 13:41:48 -0300

    header done - #1

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+export default function Header() {
+  return (
+    <header>
+      <img
+        className="logo"
+        src="./src/assets/Chef Claude Icon.svg"
+        alt="Chef Claude Icon, a robot with a cook hat."
+      />
+      <h1 className="logo-title">Chef Claude</h1>
+    </header>
+  );
+}
